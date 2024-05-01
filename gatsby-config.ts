203e04b9b7350dd1210416cac0a27a57a0dbe780
tungdd <tungdd98@gmail.com>
2024-05-01 21:42:25 +0700

    update env data

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,43 +1,43 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-	siteMetadata: {
-		title: `My Gatsby Site`,
-		siteUrl: `https://www.yourdomain.tld`,
-	},
-	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-	// If you use VSCode you can also use the GraphQL plugin
-	// Learn more at: https://gatsby.dev/graphql-typegen
-	graphqlTypegen: true,
-	plugins: [
-		{
-			resolve: 'gatsby-source-wordpress',
-			options: {
-				url: 'https://admin.demoweb.io.vn/graphql',
-			},
-		},
-		'gatsby-plugin-image',
-		'gatsby-plugin-sharp',
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-postcss',
-		// 'gatsby-plugin-google-gtag',
-		'gatsby-plugin-sitemap',
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				icon: 'src/images/icon.png',
-			},
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'images',
-				path: './src/images/',
-			},
-			__key: 'images',
-		},
-		'gatsby-plugin-react-helmet',
-	],
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        url: process.env.WPGRAPHQL_URL || 'https://admin.demoweb.io.vn/graphql',
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    // 'gatsby-plugin-google-gtag',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    'gatsby-plugin-react-helmet',
+  ],
 };
 
 export default config;
